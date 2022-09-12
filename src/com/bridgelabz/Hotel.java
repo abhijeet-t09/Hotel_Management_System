7e6d3a55304d20f95602ac7a4c7d1495b81c9076
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-12 19:45:30 +0530

    Hotel Details

---
+++ src/com/bridgelabz/Hotel.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Hotel {
+    String hotelName;
+    int rate;
+
+    public Hotel(String hotelName, int rate) {
+        this.hotelName = hotelName;
+        this.rate = rate;
+    }
+}
---
+++ src/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservation {
+    List<Hotel> hotelList = new ArrayList<>();
+
+    public void addHotel(Hotel hotel) {
+        hotelList.add(hotel);
+    }
+}
+
